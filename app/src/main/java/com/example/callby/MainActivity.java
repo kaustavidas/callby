12f9f36bf0653d997c47daf1075f528c4df6804c
Kaustav iDas <49059670+official-idas@users.noreply.github.com>
2020-11-19 15:28:03 +0530

    dialpad

--- app/src/main/java/com/example/callby/MainActivity.java
+++ app/src/main/java/com/example/callby/MainActivity.java
@@ -8,8 +8,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOne;
+    Button btnTwo;
+    Button btnThree;
+    Button btnFour;
+    Button btnFive;
+    Button btnSix;
+    Button btnSeven;
+    Button btnEight;
+    Button btnNine;
+    Button btnZero;
+    Button btnHashTag;
+    Button btnStar;
+    Button btnDial;
+    Button btnOnDelete;
+
+    EditText input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +47,24 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        btnOne = findViewById(R.id.buttonOne);
+        btnTwo = findViewById(R.id.buttonTwo);
+        btnThree = findViewById(R.id.buttonThree);
+        btnFour = findViewById(R.id.buttonFour);
+        btnFive = findViewById(R.id.buttonFive);
+        btnSix = findViewById(R.id.buttonSix);
+        btnSeven = findViewById(R.id.buttonSeven);
+        btnEight = findViewById(R.id.buttonEight);
+        btnNine = findViewById(R.id.buttonNine);
+        btnZero = findViewById(R.id.buttonZero);
+        btnStar = findViewById(R.id.buttonStar);
+        btnHashTag = findViewById(R.id.buttonHashTag);
+        btnDial = findViewById(R.id.buttonDial);
+        btnOnDelete = findViewById(R.id.buttonDelete);
+
+        input = findViewById(R.id.editTextTextPersonName);
+
     }
 
     @Override
@@ -49,4 +88,71 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void one(View v){
+        onButtonClick(btnOne, input, "1");
+    }
+
+    public void two(View v){
+        onButtonClick(btnTwo, input, "2");
+    }
+
+    public void three(View v){
+        onButtonClick(btnThree, input, "3");
+    }
+
+    public void four(View v){
+        onButtonClick(btnFour, input, "4");
+    }
+
+    public void five(View v){
+        onButtonClick(btnFive, input, "5");
+    }
+
+    public void six(View v){
+        onButtonClick(btnSix, input, "6");
+    }
+
+    public void seven(View v){
+        onButtonClick(btnSeven, input, "7");
+    }
+
+    public void eight(View v){
+        onButtonClick(btnEight, input, "8");
+    }
+
+    public void nine(View v){
+        onButtonClick(btnNine, input, "9");
+    }
+
+    public void zero(View v){
+        onButtonClick(btnZero, input, "0");
+    }
+
+    public void star(View v){
+        onButtonClick(btnStar, input, "*");
+    }
+
+    public void hashTag(View v){
+        onButtonClick(btnHashTag, input, "#");
+    }
+
+    public void onDelete(View v){
+        input.setText("");
+    }
+
+    public void call(View v){
+        if(input.getText().length() <= 3) {
+            Toast.makeText(this, "Please Enter A Valid Phone Number", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+    }
+
+    public void onButtonClick(Button button, EditText inputNumber, String number){
+        String cache = input.getText().toString();
+        inputNumber.setText(cache + number);
+
+    }
+
 }
